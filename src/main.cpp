7c96115e4b69d51e9634970244d974b9b2849b75
Stephan Roth <redsteve@gmx.de>
2025-09-09 17:28:06 +0200

    Changes to support MCP transport strategies.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "commandlineargumentparser.hpp"
 #include "mcpserver.hpp"
+#include "stdinstdoutmcptransport.hpp"
 
 const char* const APPLICATION_NAME = "SysMLv2MCPServer";
 const char* const APPLICATION_VERSION = "0.0.9 BETA";
@@ -9,7 +10,13 @@ int main(int argc, const char** argv) {
   ProgramOptions programOptions = parser.parse(argc, argv);
 
   MCPServer server { APPLICATION_NAME, APPLICATION_VERSION, programOptions };
+  server.setTransport(std::make_unique<StdinStdoutMcpTransport>());
   server.run();
+
+  // Wait for interrupt
+  while (server.isRunning()) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  }
 
   return EXIT_SUCCESS;
 }

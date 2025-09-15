caaf27eef8ca5d77cd0dffff220773a066c64fe2
Stephan Roth <redsteve@gmx.de>
2025-09-15 13:04:39 +0200

    Necessary changes due to new/changed command line parameters.

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "httpmcptransport.hpp"
 #include "mcpserver.hpp"
 #include "stdinstdoutmcptransport.hpp"
+#include "sysmlv2/sysmlv2apiclient.hpp"
 
 const char* const APPLICATION_NAME = "SysMLv2MCPServer";
 const char* const APPLICATION_VERSION = "0.0.9 BETA";
@@ -10,17 +11,27 @@ int main(int argc, const char** argv) {
   CommandLineArgumentParser parser{ APPLICATION_NAME, APPLICATION_VERSION };
   ProgramOptions programOptions = parser.parse(argc, argv);
 
+  // Step 1: Configure the server
   MCPServer server { APPLICATION_NAME, APPLICATION_VERSION, programOptions };
-  server.setTransport(std::make_unique<HttpMcpTransport>(APPLICATION_NAME, APPLICATION_VERSION));
-  // server.setTransport(std::make_unique<StdinStdoutMcpTransport>());
-  server.run();
+  if (programOptions.mcpTransportKind_ == McpTransportKind::stdinout) {
+    server.setMcpTransport(std::make_unique<StdinStdoutMcpTransport>());
+  } else {
+    server.setMcpTransport(std::make_unique<HttpMcpTransport>(APPLICATION_NAME, APPLICATION_VERSION));
+  }
 
-  // Wait for interrupt
+  // Step 2: Start the server
+  try {
+    server.run();
+  } catch (const std::runtime_error& ex) {
+    std::cerr << "FATAL ERROR - Server start failed: " << ex.what() << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  // Step 3: Wait for interrupt
   while (server.isRunning()) {
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
   }
 
   server.stop();
-  
   return EXIT_SUCCESS;
 }

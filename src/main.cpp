9004b47642541cbc47122c092404c3a52b482601
Stephan Roth <redsteve@gmx.de>
2025-09-17 11:32:10 +0200

    Added commands (function calls) for log output via apdlog.

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,13 @@
 #include "stdinstdoutmcptransport.hpp"
 #include "sysmlv2/sysmlv2apiclient.hpp"
 
+#include <spdlog/spdlog.h>
+
 const char* const APPLICATION_NAME = "SysMLv2MCPServer";
 const char* const APPLICATION_VERSION = "0.0.9 BETA";
 
 int main(int argc, const char** argv) {
+  spdlog::set_level(spdlog::level::trace);
   CommandLineArgumentParser parser{ APPLICATION_NAME, APPLICATION_VERSION };
   ProgramOptions programOptions = parser.parse(argc, argv);
 
@@ -15,15 +18,17 @@ int main(int argc, const char** argv) {
   MCPServer server { APPLICATION_NAME, APPLICATION_VERSION, programOptions };
   if (programOptions.mcpTransportKind_ == McpTransportKind::stdinout) {
     server.setMcpTransport(std::make_unique<StdinStdoutMcpTransport>());
+    spdlog::info("MCP transport configured to stdin/stdout.");
   } else {
     server.setMcpTransport(std::make_unique<HttpMcpTransport>(APPLICATION_NAME, APPLICATION_VERSION));
+    spdlog::info("MCP transport configured to HTTP.");
   }
 
   // Step 2: Start the server
   try {
     server.run();
   } catch (const std::runtime_error& ex) {
-    std::cerr << "FATAL ERROR - Server start failed: " << ex.what() << std::endl;
+    spdlog::error("FATAL ERROR - Server start failed. Reason: {}", ex.what());
     return EXIT_FAILURE;
   }
 
@@ -33,5 +38,6 @@ int main(int argc, const char** argv) {
   }
 
   server.stop();
+  spdlog::info("Server successfully stopped. Good bye!");
   return EXIT_SUCCESS;
 }
